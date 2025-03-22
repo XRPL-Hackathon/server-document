645674391fe4edd482cea30e588c119a2e424198
Coldot <41678750+Coldot@users.noreply.github.com>
2025-03-22 21:20:31 +0900

    Feat: S3 클라이언트 설정 로직 개선
    - 환경 변수에 따라 S3 클라이언트를 설정하는 로직 추가
    - 로컬 프로파일 사용 시 AWS 프로파일을 통해 세션 생성
    - 기존의 AWS 접근 키를 사용하는 방식 유지

--- src/config/S3Config.py
+++ src/config/S3Config.py
@@ -4,14 +4,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+ENV = os.getenv("ENV")
+AWS_PROFILE = os.getenv("AWS_PROFILE")
+
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_S3_BUCKET_NAME = os.getenv("AWS_S3_BUCKET_NAME")
 AWS_S3_REGION = os.getenv("AWS_S3_REGION")
 
-s3_client = boto3.client(
-    "s3",
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-    region_name=AWS_S3_REGION
-)
+if ENV == "local-profile":
+    session = boto3.Session(profile_name=AWS_PROFILE)
+    s3_client = session.client("s3")
+else:
+    s3_client = boto3.client(
+        "s3",
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        region_name=AWS_S3_REGION
+    )
